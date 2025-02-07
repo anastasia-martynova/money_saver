df556492c17b81bbe7ce031c07c9d03814571bbb
anastasia-martynova <martinovi17@mail.ru>
2025-02-07 20:27:33 +0300

    refactoring

--- frontend/server.js
+++ frontend/server.js
@@ -5,36 +5,57 @@ const { json } = require("stream/consumers");
   
 const pathToFilename = {
     "/": "index.html",
-    "/analysis": "analysis.html",
-    "/categories": "categories.html",
-    "/planning": "planning.html",
-    "/api/expenses": "expenses.json"
+    "/analysis": "analysis/analysis.html",
+    "/categories": "categories/categories.html",
+    "/planning": "planning/planning.html",
+}
+
+const data = {
+    expenses: [
+        {
+        "expenseId": "0",
+        "date": new Date(),
+        "cost": 1000,
+        "category": "grocery"
+        }
+    ]
+}
+
+function handleAPIRequests(path, response) {
+    response.setHeader("Content-Type", "application/json; charset=utf-8;")
+    if (path === '/api/expenses') {
+        response.end(JSON.stringify(data))  
+    }
+}
+
+function handleStatic(url, response) {
+    let filePath = pathToFilename[url] ?? url.substring(1)
+    fs.readFile(filePath, function(error, data){  
+        if(error){
+            response.statusCode = 404
+            fs.readFile('error/404.html', function(error, data) {
+                response.end(data)
+            });
+        }   
+        else {
+            response.end(data)
+        }
+    });
+}
+
+function isApiRequest(request) {
+    return request.url.includes('api/')
 }
 
 http.createServer(function(request, response){
-      
-    console.log(`Запрошенный адрес: ${request.url}`);
 
-    let filePath = pathToFilename[request.url] ?? request.url.substring(1)
+    if (isApiRequest(request)) {
+        handleAPIRequests(request.url, response)
+        return
+    }
 
-    fs.readFile(filePath, function(error, data){
-              
-        if(error){
-            response.statusCode = 404;
-            fs.readFile('404.html', function(error, data) {
-                response.end(data);
-            });
-        }   
-        else{
-            if (filePath == "expenses.json") {
-                console.log(JSON.stringify(JSON.parse(data)))
-                response.setHeader("Content-Type", "application/json; charset=utf-8;")
-                response.end(JSON.stringify(JSON.parse(data)))
-                return
-            }
-            response.end(data);
-        }
-    });
+    handleStatic(request.url, response)
+
 }).listen(3000, function(){
     console.log("Server started at 3000");
 });
