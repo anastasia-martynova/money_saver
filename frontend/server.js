f60541641abf2dcdb5ddd6145a459b5e99e201a2
anastasia-martynova <martinovi17@mail.ru>
2025-02-08 23:58:42 +0300

    done with main logic for adding expenses in html

--- frontend/server.js
+++ frontend/server.js
@@ -2,60 +2,71 @@ const http = require("http");
 const fs = require("fs");
 const { join } = require("path");
 const { json } = require("stream/consumers");
-  
+
 const pathToFilename = {
-    "/": "index.html",
-    "/analysis": "analysis/analysis.html",
-    "/categories": "categories/categories.html",
-    "/planning": "planning/planning.html",
-}
+  "/": "index.html",
+  "/analysis": "analysis/analysis.html",
+  "/categories": "categories/categories.html",
+  "/planning": "planning/planning.html",
+};
 
 const data = {
-    expenses: [
-        {
-        "expenseId": "0",
-        "date": new Date(),
-        "cost": 1000,
-        "category": "grocery"
-        }
-    ]
-}
+  expenses: [
+    {
+      expenseId: "0",
+      date: new Date("2025-01-17"),
+      cost: 1000,
+      category: "grocery",
+    },
+    {
+      expenseId: "1",
+      date: new Date(),
+      cost: 500,
+      category: "grocery",
+    },
+    {
+        expenseId: "2",
+        date: new Date(),
+        cost: 700,
+        category: "grocery",
+      }
+  ],
+};
 
 function handleAPIRequests(path, response) {
-    response.setHeader("Content-Type", "application/json; charset=utf-8;")
-    if (path === '/api/expenses') {
-        response.end(JSON.stringify(data))  
-    }
+  response.setHeader("Content-Type", "application/json; charset=utf-8;");
+  if (path === "/api/expenses") {
+    response.end(JSON.stringify(data));
+  }
 }
 
 function handleStatic(url, response) {
-    let filePath = pathToFilename[url] ?? url.substring(1)
-    fs.readFile(filePath, function(error, data){  
-        if(error){
-            response.statusCode = 404
-            fs.readFile('error/404.html', function(error, data) {
-                response.end(data)
-            });
-        }   
-        else {
-            response.end(data)
-        }
-    });
+  let filePath = pathToFilename[url] ?? url.substring(1);
+  fs.readFile(filePath, function (error, data) {
+    if (error) {
+      response.statusCode = 404;
+      fs.readFile("error/404.html", function (error, data) {
+        response.end(data);
+      });
+    } else {
+      response.end(data);
+    }
+  });
 }
 
 function isApiRequest(request) {
-    return request.url.includes('api/')
+  return request.url.includes("api/");
 }
 
-http.createServer(function(request, response){
-
+http
+  .createServer(function (request, response) {
     if (isApiRequest(request)) {
-        handleAPIRequests(request.url, response)
-        return
+      handleAPIRequests(request.url, response);
+      return;
     }
 
-    handleStatic(request.url, response)
-
-}).listen(3000, function(){
+    handleStatic(request.url, response);
+  })
+  .listen(3000, function () {
     console.log("Server started at 3000");
-});
+  });
--- frontend/test.js
+++ frontend/test.js
@@ -1,7 +1,42 @@
+const dateOptions = {month: 'long', day: 'numeric'}
+
+function createAndAddNewExpense(expenseId, date, cost, category){
+  const newDiv = document.createElement("div")
+  const newSpanDate = document.createElement("span")
+  const newSpanPrice = document.createElement("span")
+  const newSpanIconWrapper = document.createElement("span")
+  const newImgIcon = document.createElement("img")
+
+  newDiv.className = "expenses_list__item_wrapper"
+  newSpanDate.className = "expenses_list__item_date"
+  newSpanPrice.className = "expenses_list__item_price"
+  newSpanIconWrapper.className = "expenses_list__item_icon_wrapper"
+  newImgIcon.className = "expenses_list__item_icon_image"
+  newImgIcon.src = "assets/shopping_cart.png"
+
+  newDiv.appendChild(newSpanDate)
+  newDiv.appendChild(newSpanPrice)
+  newDiv.appendChild(newSpanIconWrapper)
+  newSpanIconWrapper.appendChild(newImgIcon)
+
+  const currentDiv = document.getElementsByClassName("expenses_list__wrapper_list")[0]
+  currentDiv.appendChild(newDiv)
+
+  let newDate = new Date(date)
+  document.getElementsByClassName("expenses_list__item_date")[expenseId].textContent = newDate.toLocaleDateString("en-US", dateOptions)
+  document.getElementsByClassName("expenses_list__item_price")[expenseId].textContent = `${cost} р`
+}
+
+function addAllExpensesToHtml(data){
+  data.expenses.forEach(expense => {
+    createAndAddNewExpense(expense.expenseId, expense.date, expense.cost, expense.category)
+  });
+}
+
 fetch('http://localhost:3000/api/expenses')
   .then((response) => {
-    return response.json();
+    return response.json()
   })
   .then((data) => {
-    console.log(data["05.02.2025"]);
+    addAllExpensesToHtml(data)
   });
